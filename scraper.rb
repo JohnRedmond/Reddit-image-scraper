a23a649bc080aa428cb0b22d49733b980864f381
John Redmond <johnnyredman89@gmail.com>
2012-07-17 23:14:29 -0400

    Added Comments

--- scraper.rb
+++ scraper.rb
@@ -4,22 +4,26 @@ require 'open-uri'
 require 'restclient'
 
 class Scraper
-  
+
+#Asks user to choose subreddit and directory to save to
   puts "Choose SubReddit:"
   subreddit = gets
-  puts "Folder to save too"
-  folder = gets
+  puts "Directory to save to"
+  direc = gets
 
-  Dir.mkdir(folder) unless File.directory? folder
+#Creates directory if the directory doesn't already exist
+  Dir.mkdir direc unless File.directory? direc
 
+#Gets subreddit and pulls out image links
   doc = open("http://www.reddit.com/r/#{subreddit}") { |f| Hpricot(f) }
   links = doc.search("//a[@class='title ']")
   links.map! { |link| "#{link['href']}" }
 
+#If link does not have a blank file extension then save file
   links.each do |link|
     if File.extname(link) != "" 
-      File.open("#{folder}/" + File.basename(link), 'w') do
-        |img| img.write(RestClient.get(link)) 
+      File.open("#{direc}/" + File.basename(link), 'w') do
+        |i| i.write(RestClient.get(link))
       end    
     end
   end
